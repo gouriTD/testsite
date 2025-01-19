b8d2adbe62300d2e0bd2859e7744177a017f6590
Gouri Tripathi <gtripathi@adobe.com>
2025-01-19 20:22:33 +0530

    banner added

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -3,15 +3,15 @@ function decorateAccordion(el) {
     console.log(titles)
     titles.forEach((title) => {
       // Add a class to the title container
-      title.classList.add('item-title');
+      title.classList.add('item-text');
       // Remove the empty div
       title.querySelector(':scope > div:last-of-type').remove();
       // Add a class to the content
-      title.nextElementSibling.classList.add('item-content');
+      title.nextElementSibling.classList.add('item-image');
       // Add a click handler to open the content
-      title.addEventListener('click', () => {
-        title.classList.toggle('open');
-      });
+    //   title.addEventListener('click', () => {
+    //     title.classList.toggle('open');
+    //   });
     });
   }
   
